fbe662d275c92d211e3698848540903663d504a0
abang idris <abangmuhammadidris@gmail.com>
2023-08-12 12:06:21 +0700

    materi laravel ke 3

---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/contact', function(){
+    return view('<h1>Daftar Kontak</h1>');
+});
+Route::get('/contact/create', function(){
+    return view('<h1>Tambah Kontak Baru</h1>');
+});
+Route::get('/contacts/{id}', function($id) {
+   return "Ini Kontak ke-" . $id;
+});
+Route::get('/companies/{name?}', function($name=null) {
+   if($name) {
+       return "Nama Perusahaan: " . $name;
+   } else {
+       return "Nama Perusahaan Kosong";
+   }
+});
